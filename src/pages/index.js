fe7fefa08694e0059ed08df57651a96a7c873090
naposalvo1972 <salvo.napo1972@gmail.com>
2021-12-11 16:08:38 +0100

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ const links = [
     text: "Tutorial",
     url: "https://www.gatsbyjs.com/docs/tutorial/",
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+      "Un grande posto to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
     color: "#E95800",
   },
   {
