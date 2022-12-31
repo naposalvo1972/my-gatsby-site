ed0f0145316b7575871556b19d7b6d257736fce2
naposalvo1972 <naposalvo1972@gmail.com>
2022-12-31 16:27:05 +0100

    aa

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,48 @@
 const path = require("path")
 const axios = require("axios")
 
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          id
+          frontmatter {
+            slug
+          }
+          internal {
+            contentFilePath
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild('Error loading MDX result', result.errors)
+  }
+
+  // Create blog post pages.
+  const posts = result.data.allMdx.nodes
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  // you'll call `createPage` for each result
+  posts.forEach(node => {
+    createPage({
+      // As mentioned above you could also query something else like frontmatter.title above and use a helper function
+      // like slugify to create a slug
+      path: node.frontmatter.slug,
+      // Provide the path to the MDX content file so webpack can pick it up and transform it into JSX
+      //component: node.internal.contentFilePath,
+      component: `${postTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
+      // You can use the values in this context in
+      // our page layout component
+      context: { id: node.id },
+    })
+  })
+}
+
 exports.createPages = ({ actions }) => {
   const { createPage } = actions
   return new Promise((resolve, reject) => {
